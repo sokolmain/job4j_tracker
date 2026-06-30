49f5281eadfd6fdab29baa9b42e6f5d0ec73f983
Sokolmain <v-s102@mail.ru>
2026-06-30 16:21:54 +0500

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeofempty = 10;
+    public String surname;
+    public static final String NEW_VALUE = "";
+    String mame;
+
+    Broken() {
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+}

44131f01d133e63a1011351a0f7324ee88477c69
Sokolmain <v-s102@mail.ru>
2026-07-22 22:24:04 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,8 +4,10 @@ import java.time.LocalDateTime;
 
 public class Item {
     private int id;
+
     private String name;
-    private LocalDateTime created =  LocalDateTime.now();
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -17,10 +19,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public int getId() {
@@ -37,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }

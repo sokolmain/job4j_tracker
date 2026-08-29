04f30176f2a0b733e8db7f455c2ef8ae501f8b11
Sokolmain <v-s102@mail.ru>
2026-08-29 11:58:06 +0500

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import ru.job4j.input.ConsoleInput;
+
+public class ValidateInput extends ConsoleInput {
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = 1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Пожалуйста, введите корректные данные");
+            }
+        } while (invalid);
+        return value;
+    }
+}
